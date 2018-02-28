d5f73215262a977bb55690d017f24453033f18eb
Tanya Kavrakova <tanya.kavrakova@gmail.com>
2018-02-28 15:57:20 +0200

    Remove obsolete functions
    Some of the sunctions in socket.js were created for testing purposes.
    Now for the current state of the application, these functions are
    onsolete, so they are removed.

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -54,21 +54,21 @@ let socket = new Socket("/socket", {})
 
 socket.connect()
 
-function new_channel(subtopic, screen_name) {
-  return socket.channel("game:" + subtopic, {screen_name: screen_name});
-}
+//function new_channel(subtopic, screen_name) {
+//  return socket.channel("game:" + subtopic, {screen_name: screen_name});
+//}
 
-var game_channel = new_channel("moon", "moon")
-function join(channel) {
-  channel.join()
-    .receive("ok", response => {
-      console.log("Joined successfully!", response)
-    })
-  .receive("error", response => {
-    console.log("Unable to join", response)
-  })
-}
-join(game_channel)
+//var game_channel = new_channel("moon", "moon")
+//function join(channel) {
+//  channel.join()
+//    .receive("ok", response => {
+//      console.log("Joined successfully!", response)
+//    })
+//  .receive("error", response => {
+//    console.log("Unable to join", response)
+//  })
+//}
+//join(game_channel)
 
   // Now that you are connected, you can join channels with a topic:
 //let channel = socket.channel("topic:subtopic", {})
@@ -76,61 +76,23 @@ join(game_channel)
 //  .receive("ok", resp => { console.log("Joined successfully", resp) })
 //  .receive("error", resp => { console.log("Unable to join", resp) })
 
-function leave(channel) {
-  channel.leave()
-    .receive("ok", response => {
-      console.log("Left successfully", response)
-    })
-  .receive("error", response => {
-    console.log("Unable to leave", response)
-  })
-}
+//function leave(channel) {
+//  channel.leave()
+//    .receive("ok", response => {
+//      console.log("Left successfully", response)
+//    })
+//  .receive("error", response => {
+//    console.log("Unable to leave", response)
+//  })
+//}
 
-function say_hello(channel, greeting) {
-  channel.push("hello", {"message": greeting})
-    .receive("ok", response => {
-      console.log("Hello", response.message)
-    })
-  .receive("error", response => {
-    console.log("Unable to say hello to the channel.", response.message)
-  })
-}
 
-function new_game(channel) {
-  channel.push("new_game")
-    .receive("ok", response => {
-      console.log("New Game!", response)
-    })
-  .receive("error", response => {
-    console.log("Unable to start a new game.", response)
-  })
-}
-
-function add_player(channel, player) {
-  channel.push("add_player", player)
-    .receive("error", response => {
-      console.log("Unable to add new player: " + player, response)
-    })
-}
-
-//function guess_number(channel, )
-
-game_channel.on("player_added", response => {
-  console.log("Player Added", response)
-})
-
-game_channel.on("said_hello", response => {
-  console.log("Hello Said", response)
-})
-
-say_hello(game_channel, "World!")
-
-window.App = {
-  new_game: new_game,
-  add_player: add_player,
-  game_channel: game_channel,
-  socket: socket,
-  say_hello: say_hello,
-  join: join
-}
+//#window.App = {
+//#  new_game: new_game,
+//#  add_player: add_player,
+//#  game_channel: game_channel,
+//#  socket: socket,
+//#  say_hello: say_hello,
+//#  join: join
+//#}
 export default socket
